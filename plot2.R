b05ac8b2796617b36cb0f87bf348f4b9f8cd9ebb
jimitsoni <jimit.soni@gmail.com>
2015-04-12 23:35:38 +0530

    Adding Code

---
+++ plot2.R
@@ -0,0 +1,19 @@
+## Getting full dataset
+full_data <- read.csv("/home/jimit/household_power_consumption.txt", header=T, sep=';', na.strings="?",
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_data$Date <- as.Date(full_data$Date, format="%d/%m/%Y")
+
+## Extracting Subset of the data based on date given
+data <- subset(full_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(full_data)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+
+## Saving to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
